8f71f22edb0722a008a47eb99a628276773bb405
sho ochiai <ochi1020@gmail.com>
2023-04-20 22:10:03 +0900

    feat: implement basic functionality of grep

--- cmd/root.go
+++ cmd/root.go
@@ -22,11 +22,19 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
+
+type GrepResult struct {
+	LineNumber int
+	Line       string
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -53,6 +61,59 @@ to quickly create a Cobra application.`,
 		if _, err := os.Stat(path); os.IsNotExist(err) {
 			fmt.Printf("gogrep: no matches found: %s", path)
 			return
+		}
+
+		result := make(map[string][]GrepResult)
+
+		err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+
+			if err != nil {
+				return err
+			}
+
+			if info.IsDir() {
+				return nil
+			}
+
+			file, err := os.Open(path)
+
+			if err != nil {
+				return nil
+			}
+
+			defer file.Close()
+
+			maches := []GrepResult{}
+
+			scanner := bufio.NewScanner(file)
+			i := 1
+			for scanner.Scan() {
+				line := scanner.Text()
+				if strings.Contains(line, keyword) {
+					gr := GrepResult{
+						LineNumber: i,
+						Line:       line,
+					}
+					maches = append(maches, gr)
+				}
+				i += 1
+			}
+			if len(maches) > 0 {
+				result[path] = maches
+			}
+
+			return nil
+		})
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		for k, v := range result {
+			fmt.Println(k)
+			for _, gr := range v {
+				fmt.Println(gr.LineNumber, gr.Line)
+			}
 		}
 	},
 }
